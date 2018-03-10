5bc0608b19a2e57930e8039d5ac4638d429f4652
Aparna Dalal <34259886+aparnada7@users.noreply.github.com>
2018-03-09 22:09:29 -0800

    server.py for chat server
    server.py for chat server

---
+++ Lab3/server.py
@@ -0,0 +1,28 @@
+import zmq
+
+# ZeroMQ Context
+context = zmq.Context()
+
+# Define the socket using the "Context"
+# This socket is for receiving all the messages.
+#RECEIVE 
+receiverSocket = context.socket(zmq.SUB)
+receiverSocket.setsockopt_string(zmq.SUBSCRIBE, "")
+receiverSocket.bind("tcp://127.0.0.1:1234")
+
+# This socket is for publishing/sending all the messages.
+#SEND
+senderSocket = context.socket(zmq.PUB)
+senderSocket.bind("tcp://127.0.0.1:7777")
+
+print("Server started...")
+
+while True:
+    #Handling messages received from client
+    chatMsgRcvd = receiverSocket.recv().decode()
+    chatUserRcvd = receiverSocket.recv().decode()
+    #Printing chat status on server for overall visiblity/clarity
+    print("[{}]: {}".format(chatUserRcvd, chatMsgRcvd))
+    #Publishing received chat/user info to clients
+    senderSocket.send_string(chatMsgRcvd)
+    senderSocket.send_string(chatUserRcvd)
