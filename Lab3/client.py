d49018946dd14dcbf82408a123ae9882992bb093
Aparna Dalal <34259886+aparnada7@users.noreply.github.com>
2018-03-09 22:02:24 -0800

    client.py for Chat server
    client.py for Chat server

---
+++ Lab3/client.py
@@ -0,0 +1,50 @@
+import zmq
+import threading
+import sys
+
+# ZeroMQ Context
+context = zmq.Context()
+initiator = sys.argv[1]
+
+# Define the socket using the "Context"
+# This socket is for publishing/sending all the messages.
+#SEND
+senderSocket = context.socket(zmq.PUB)
+senderSocket.connect("tcp://127.0.0.1:1234")
+
+# This socket is for receiving all the messages.
+#RECEIVE
+receiverSocket = context.socket(zmq.SUB)
+receiverSocket.connect("tcp://127.0.0.1:7777")
+receiverSocket.setsockopt_string(zmq.SUBSCRIBE, "")
+
+print("User[{}] Connected to the chat server.".format(initiator))
+
+
+#Function to handle incoming messages, published by server to clients.
+def chat_handler():
+    while True:
+        chatMsg = receiverSocket.recv()
+        lastChatUser = receiverSocket.recv()
+        chatMsg = chatMsg.decode()
+        lastChatUser = lastChatUser.decode()
+        # Duplicating initiator's own message in chat window is not required.
+        # Hence, pass if initiator is same as lastChatUser.
+        if lastChatUser == initiator:
+            pass
+        else:
+            print("\n[{}]: {} \n[{}] > ".format(lastChatUser, chatMsg, initiator),  end="")
+            
+
+#Defining thread so that message receiving/chat handling on client side is continuous        
+def thread():
+    thread = threading.Thread(target=chat_handler)
+    thread.start()
+
+
+thread()
+while True:
+    inputChatMsg = input("[{}] > ".format(initiator))
+    #Send chat message and initiator info to server
+    senderSocket.send(inputChatMsg.encode())
+    senderSocket.send(initiator.encode())
